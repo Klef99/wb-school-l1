f45e0bc7aa13c015caad8a0de261b5566b4173f8
Egor Yakovlev <yakovlev.egor.o@gmail.com>
2025-09-15 17:53:03 +0300

    task 13

--- task-13/main.go
+++ task-13/main.go
@@ -1,1 +1,27 @@
-package task_13
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b int
+
+	fmt.Scan(&a, &b)
+
+	// Метод простой, но может привести к переполнению типа при слишком больших значениях
+	a += b
+	b = a - b
+	a = a - b
+
+	fmt.Println("First swap")
+	fmt.Println(a, b)
+
+	// Свап через XOR обмен более предпочтителен из-за отсутствия проблем с размерами типа int
+	a ^= b
+	b ^= a
+	a ^= b
+
+	fmt.Println("Second swap")
+	fmt.Println(a, b)
+}
