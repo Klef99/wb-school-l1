6e46351591339090bf2d30757df5772e7b1d32c0
Egor Yakovlev <yakovlev.egor.o@gmail.com>
2025-09-15 17:22:07 +0300

    task 10

--- task-10/main.go
+++ task-10/main.go
@@ -1,1 +1,20 @@
-package task_10
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	res := make(map[int][]float64, 10)
+
+	data := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+
+	for _, v := range data {
+		// получим значения десятков для определения группы
+		d := 10 * int(v/10)
+		// Несмотря на nil слайсы в мапе, операция append безопасна для использования.
+		res[d] = append(res[d], v)
+	}
+
+	fmt.Println(res)
+}
