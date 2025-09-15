85b1d034a0882a6e39808b6f650a20e48bd6f1b1
Egor Yakovlev <yakovlev.egor.o@gmail.com>
2025-09-15 17:56:04 +0300

    task 14

--- task-14/main.go
+++ task-14/main.go
@@ -1,1 +1,35 @@
-package task_14
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func getType(v interface{}) string {
+	t := reflect.TypeOf(v)
+	if t == nil {
+		return "unknown"
+	}
+	// для определения канала не прибегая к конкретному типу (chan int и т.д.),
+	// пришлось использовать reflect вместо switch.(type)
+	switch t.Kind() {
+	case reflect.Int:
+		return "int"
+	case reflect.String:
+		return "string"
+	case reflect.Bool:
+		return "bool"
+	case reflect.Chan:
+		return "chan"
+	default:
+		return "unknown"
+	}
+}
+
+func main() {
+	ch := make(chan int)
+	i := 1
+	s := "str"
+	b := true
+	fmt.Println(getType(i), getType(b), getType(ch), getType(s))
+}
