55a19a01b162152fc337a5d3eee49821ddb2f413
Egor Yakovlev <yakovlev.egor.o@gmail.com>
2025-09-15 17:38:30 +0300

    task 12

--- task-12/main.go
+++ task-12/main.go
@@ -1,1 +1,26 @@
-package task_12
+package main
+
+import (
+	"fmt"
+)
+
+func set[T comparable](elems ...T) []T {
+	res := make([]T, len(elems))
+	data := make(map[T]struct{}, len(elems))
+
+	for _, v := range elems {
+		data[v] = struct{}{}
+	}
+
+	for k := range data {
+		res = append(res, k)
+	}
+
+	return res
+}
+
+func main() {
+	data := []string{"cat", "cat", "dog", "cat", "tree", "bush"}
+
+	fmt.Println(set(data...))
+}
